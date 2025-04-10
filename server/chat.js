8d59d15621db8b18af027dbdbe3c0e69506605bf
xuzc <xuzc@yxt.com>
2025-04-10 19:46:58 +0800

    feat：代码初提

--- server/chat.js
+++ server/chat.js
@@ -3,5 +3,5 @@ var dbserver = require('../dao/dbserver.js'); // 引入数据操作模块
 exports.msg = function (req, res) {
     console.log('聊天请求接收:', req.body); // 打印请求体
     let data = req.body; // 获取请求体
-    dbserver.chat(data, res); // 调用查询用户函数
+    dbserver.msg(data, res); // 调用查询用户函数
 }
